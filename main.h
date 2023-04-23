bca647119ed80bce3ba6564e275f7976c42e7449
KM8Oz <ouazmir@gmail.com>
2023-04-23 08:03:30 -0700

    main.h add get_flags func

--- main.h
+++ main.h
@@ -19,12 +19,6 @@
 #define S_SHORT 1
 int _printf(const char *format, ...);
 int _putchar(char c);
-int get_flag(const char *format, int *i)
-/* FLAGS */
-#define F_MINUS 1
-#define F_PLUS 2
-#define F_ZERO 4
-#define F_HASH 8
-#define F_SPACE 16
+int get_flag(const char *format, int *i);
 
 #endif/* MAIN_H*/
