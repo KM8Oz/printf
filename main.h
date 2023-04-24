b89d663206fd0c667a368536533a7f80b54f7a9b
KM8Oz <ouazmir@gmail.com>
2023-04-24 15:53:56 -0700

    testfolder

--- main.h
+++ main.h
@@ -8,26 +8,12 @@
 #include <strings.h>
 #include <ctype.h>
 #include <stdlib.h>
-
-/* getlen */
 int getlen(char *f);
-
-/* _strrev */
 void _strrev(char *str);
-
-/* _itoa */
 char *_itoa(int value, char *str, int base);
-
-/* _putchar */
 int _putchar(char c);
-
-/* _putstr */
 int _putstr(char *c);
-
-/* _putnum */
 int _putnum(int c);
-
-/* _printf */
 int _printf(const char *format, ...);
 
 #endif
